71df9b49cb560976a140d534c859a24518dc4e07
Szymon Sikora <sikora.szymon0@gmail.com>
2020-11-28 13:04:16 +0100

    Task done

--- Zadanie Kalkulator.py
+++ Zadanie Kalkulator.py
@@ -1,25 +1,28 @@
 import logging
+
 logging.basicConfig(level=logging.INFO)
-def count(a, b, action):
+
+
+def calculate(a, b, action):
     if action == 1:
-        return a + b
+        logging.info("Dodaję %s i %s" % (first_number, second_number))
+        return a + b,
     elif action == 2:
+        logging.info("Odejmuję %s od %s" % (second_number, first_number))
         return a - b
     elif action == 3:
+        logging.info("Mnożę %s przez %s" % (first_number, second_number))
         return a * b
     elif action == 4:
-        return (a // b)
+        logging.info("Dzielę %s przez %s" % (first_number, second_number))
+        return a / b
+    else:
+        return ValueError
+
+
 
 if __name__ == "__main__":
     action = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
     first_number = int(input("Podaj składnik 1:"))
     second_number = int(input("Podaj składnik 2:"))
-    if action == 1:
-        logging.info("Dodaję %s i %s" % (first_number, second_number))
-    elif action == 2:
-        logging.info("Odejmuję %s od %s" % (second_number, first_number))
-    elif action == 3:
-        logging.info("Mnożę %s przez %s" % (first_number, second_number))
-    elif action == 4:
-        logging.info("Dzielę %s przez %s" % (first_number, second_number))
-    print(count(first_number, second_number, action))
+    print(calculate(first_number, second_number, action))
