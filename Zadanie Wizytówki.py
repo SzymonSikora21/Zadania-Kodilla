4a9d6e2c65cb926416fedc0ca82fa5d60d11c4f0
Szymon Sikora <sikora.szymon0@gmail.com>
2020-11-29 14:21:06 +0100

    Task Done

--- Zadanie Wizytówki.py
+++ Zadanie Wizytówki.py
@@ -1,12 +1,14 @@
 import faker
-import string
+import typing
+import logging
+import random
 
-faker = faker.Faker()
+fake = faker.Faker()
 
 
 class BaseContact:
 
-    def __init__(self, name: str, surname: str, phone_number: str, email: str):
+    def __init__(self, name: str, surname: str, phone_number: int, email: str):
         self.name = name
         self.surname = surname
         self.phone_number = phone_number
@@ -29,7 +31,7 @@ class BusinessContact(BaseContact):
                  email: str,
                  work_position: str,
                  company: str,
-                 work_phone_number: str
+                 work_phone_number: int
     ):
         super().__init__(name, surname, phone_number, email)
         self.work_position = work_position
@@ -38,3 +40,51 @@ class BusinessContact(BaseContact):
 
     def contact(self):
         print(f"Wybieram numer {self.work_phone_number} i dzwonię do {self.name} {self.surname}")
+
+
+def create_contacts(contact_type: typing.ClassVar, amount: int):
+    contact_list = []
+
+    for _ in range(amount):
+        name, surname = fake.first_name(), fake.last_name()
+        phone_number = fake.random_int(100000000, 999999999)
+        email = fake.email()
+        if contact_type == BusinessContact:
+            work_position = fake.job()
+            company = fake.company()
+            work_phone_number = fake.random_int(100000000, 999999999)
+            contact_list.append(
+                BusinessContact(
+                    name=name,
+                    surname=surname,
+                    phone_number=phone_number,
+                    email=email,
+                    company=company,
+                    work_position=work_position,
+                    work_phone_number=work_phone_number
+                )
+            )
+        elif contact_type == BaseContact:
+            contact_list.append(
+                BaseContact(
+                    name=name,
+                    surname=surname,
+                    phone_number = phone_number,
+                    email=email
+                )
+            )
+        else:
+            logging.warning(f"Unsupported contact type: {contact_type}, valid types are: {BaseContact}, {BusinessContact}")
+
+    return contact_list
+
+if __name__ == "__main__":
+    contacts = []
+    contacts += create_contacts(BusinessContact, 4)
+    contacts += create_contacts(BaseContact, 4)
+
+    random.shuffle(contacts)
+
+    for contact in contacts:
+        print(contact.label_length, end=" ")
+        contact.contact()
